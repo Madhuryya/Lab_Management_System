a3632692c33eb1ec0637e60fb3613e8d0d7e0d34
HARIHARAN B P <bphariharan1301@gmail.com>
2023-06-11 13:20:23 +0530

    Update: Issue resolved

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,13 @@
-from django.urls import path,include
-from . import views
-from .views import ItemViewSet
-from .views import postItem, getItem
+from django.urls import path
+
+from .views import getItem
 
 
 
 
 urlpatterns = [
 	#  path('', views.ApiOverview, name='home')
-	 path('getData',getItem.as_view),
+	 path('getData',getItem.as_view()),
 	# path('getData/', getItem.as_view()),
     # path('postData/', postItem.as_view()),
 ]
