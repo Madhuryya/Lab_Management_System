de6e59d3dbfd2f8a651d096145e54b38c86b1ed9
Madhuryya <119807597+Madhuryya@users.noreply.github.com>
2023-07-13 22:18:18 +0530

    done

--- api/urls.py
+++ api/urls.py
@@ -5,9 +5,11 @@ from .views import getItem
 
 
 
+
 urlpatterns = [
 	#  path('', views.ApiOverview, name='home')
 	 path('getData',getItem.as_view()),
+    
 	# path('getData/', getItem.as_view()),
     # path('postData/', postItem.as_view()),
 ]
